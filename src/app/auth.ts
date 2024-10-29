341848493f6f468a95be9a09a478cae2b8375e6d
NathanRVazquez <nathanrvazquez@gmail.com>
2024-10-29 11:00:15 -0400

    edited session headers

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -23,17 +23,17 @@ const handler = NextAuth({
       authorize: async (credentials) => {
         let user = null
  
-        // logic to salt and hash password
-        const pwHash = saltAndHashPassword(credentials.password)
+        // // logic to salt and hash password
+        // const pwHash = saltAndHashPassword(credentials.password)
  
-        // logic to verify if the user exists
-        user = await getUserFromDb(credentials.email, pwHash)
+        // // logic to verify if the user exists
+        // user = await getUserFromDb(credentials.email, pwHash)
  
-        if (!user) {
-          // No user found, so this is their first attempt to login
-          // meaning this is also the place you could do registration
-          throw new Error("User not found.")
-        }
+        // if (!user) {
+        //   // No user found, so this is their first attempt to login
+        //   // meaning this is also the place you could do registration
+        //   throw new Error("User not found.")
+        // }
  
         // return user object with their profile data
         return user
--- src/components/ui/session-header.tsx
+++ src/components/ui/session-header.tsx
@@ -1,8 +1,9 @@
 'use client';
 import React from "react";
 import { signIn,signOut, useSession } from "next-auth/react";
-// import {signIn, signOut} from "next-auth/react"
-// import {auth,signIn,signOut}from "../../app/auth";
+import {Button} from "@/components/ui/button";
+
+
 
 
 const SessionHeader = () => {
@@ -25,18 +26,40 @@ const SessionHeader = () => {
   if (status === 'loading') {
     return <div>Loading...</div>;
   }
+
+
+
   return (
     <header>
-      {session ? (
-        <>
-          <p>Welcome, {session.user?.email || "User"}</p>
-          <button onClick={handleLogOutClick}>Logout</button>
-        </>
-      ) : (
+        <div className="p-4">
+        <div className="flex justify-between items-center">
+          <div>
+           
+              <a href="/"> <h1>TutorEasy</h1></a>
+          </div>
+          <div>
+            {session ? (
+              <>
+                <p>Welcome, {session.user?.email || "User"}</p>
+              </>
+            ) : (
 
-        <button onClick={handleLoginClick}>Login Here</button>
-        // <p>You are not logged in</p>
+              <p className="text-red-500">You are not logged in</p>
+            )}
+          </div>
+          <div>
+            {session ? (
+              <>
+                {/* <button className="bg-blue-400 hover:bg-blue-700" onClick={handleLogOutClick}>Logout</button> */}
+
+              </>
+            ) : (
+              // <Button className="bg-blue-400 hover:bg-blue-700" onClick={handleLoginClick}>Login Here</Button>
+              <a href="/signin"> <Button className="bg-blue-400 hover:bg-blue-700" onClick={handleLoginClick}>Login Here</Button></a>
       )}
+       </div>
+      </div>
+  </div>
     </header>
   );
 };
