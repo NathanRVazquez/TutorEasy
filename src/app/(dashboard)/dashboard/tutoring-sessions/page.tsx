b234ed8acd855d487ef6c3221a4cff868a819106
Shohruz Ernazarov <shohruzernazarov04@gmail.com>
2024-11-18 00:19:55 -0500

    Fix internal code structure of form component and page

--- src/app/(dashboard)/dashboard/tutoring-sessions/page.tsx
+++ src/app/(dashboard)/dashboard/tutoring-sessions/page.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import TutorForm from "@/components/TutoringSessionForm";
 
 const TutoringSessionsPage = () => {
-  return <TutorForm />;
+  return (
+    <div className="bg-white rounded-md p-4 drop-shadow-2xl">
+      <TutorForm />
+    </div>
+  );
 };
 
 export default TutoringSessionsPage;
--- src/components/TutoringSessionForm.tsx
+++ src/components/TutoringSessionForm.tsx
@@ -125,216 +125,208 @@ export default function TutoringSessionForm() {
 
   return (
     <Form {...form}>
-      <div className="bg-white rounded-md p-4 drop-shadow-2xl">
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-          <div className="flex flex-col lg:flex-row justify-start lg:justify-between items-start lg:items-center gap-2">
-            <FormField
-              control={form.control}
-              name={"name"}
-              render={({ field }) => (
-                <FormItem className="w-full lg:w-1/2">
-                  <FormLabel className="font-bold">TA Name</FormLabel>
-                  <FormControl>
-                    <Select onValueChange={field.onChange}>
-                      <SelectTrigger>
-                        <SelectValue placeholder="TA" {...field} />
-                      </SelectTrigger>
-                      <SelectContent>
-                        <SelectItem value="Nathan Vazquez">
-                          Nathan Vazquez
-                        </SelectItem>
-                        <SelectItem value="Ynalois Pangilinan">
-                          Ynalois Pangilinan
-                        </SelectItem>
-                        <SelectItem value="Shohruz Ernazarov">
-                          Shohruz Ernazarov
-                        </SelectItem>
-                      </SelectContent>
-                    </Select>
-                  </FormControl>
-                  <FormDescription className="font-bold">
-                    Select your name
-                  </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            <div className="flex flex-col lg:flex-row lg:justify-start justify-center items-center gap-2 lg:gap-4 xl:gap-8">
-              <FormField
-                control={form.control}
-                name="start_time"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="font-bold">
-                      Shift Start Time
-                    </FormLabel>
-                    <DatetimePicker
-                      {...field}
-                      format={[
-                        ["months", "days", "years"],
-                        ["hours", "minutes", "am/pm"],
-                      ]}
-                    />
-                    <FormDescription className="font-bold">
-                      Select your start shift time.
-                    </FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="end_time"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="font-bold">Shift End Time</FormLabel>
-                    <DatetimePicker
-                      {...field}
-                      format={[
-                        ["months", "days", "years"],
-                        ["hours", "minutes", "am/pm"],
-                      ]}
-                    />
-                    <FormDescription className="font-bold">
-                      Select your end shift time.
-                    </FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
-          </div>
-
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+        <div className="flex flex-col lg:flex-row justify-start lg:justify-between items-start lg:items-center gap-2">
           <FormField
             control={form.control}
-            name={"students"}
+            name={"name"}
             render={({ field }) => (
-              <FormItem className="w-full">
-                <FormLabel className="font-bold">Students Tutored</FormLabel>
+              <FormItem className="w-full lg:w-1/2">
+                <FormLabel className="font-bold">TA Name</FormLabel>
                 <FormControl>
-                  <Input
-                    type="number"
-                    placeholder="How many students did you tutor?"
-                    {...field}
-                  />
+                  <Select onValueChange={field.onChange}>
+                    <SelectTrigger>
+                      <SelectValue placeholder="TA" {...field} />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="Nathan Vazquez">
+                        Nathan Vazquez
+                      </SelectItem>
+                      <SelectItem value="Ynalois Pangilinan">
+                        Ynalois Pangilinan
+                      </SelectItem>
+                      <SelectItem value="Shohruz Ernazarov">
+                        Shohruz Ernazarov
+                      </SelectItem>
+                    </SelectContent>
+                  </Select>
                 </FormControl>
-                {/* <FormDescription className="font-bold">
-                  Enter the amount of students you tutored
-                </FormDescription> */}
+                <FormDescription className="font-bold">
+                  Select your name
+                </FormDescription>
                 <FormMessage />
               </FormItem>
             )}
           />
 
+          <div className="flex flex-col lg:flex-row lg:justify-start justify-center items-center gap-2 lg:gap-4 xl:gap-8">
+            <FormField
+              control={form.control}
+              name="start_time"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="font-bold">Shift Start Time</FormLabel>
+                  <DatetimePicker
+                    {...field}
+                    format={[
+                      ["months", "days", "years"],
+                      ["hours", "minutes", "am/pm"],
+                    ]}
+                  />
+                  <FormDescription className="font-bold">
+                    Select your start shift time.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="end_time"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="font-bold">Shift End Time</FormLabel>
+                  <DatetimePicker
+                    {...field}
+                    format={[
+                      ["months", "days", "years"],
+                      ["hours", "minutes", "am/pm"],
+                    ]}
+                  />
+                  <FormDescription className="font-bold">
+                    Select your end shift time.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+        </div>
+
+        <FormField
+          control={form.control}
+          name={"students"}
+          render={({ field }) => (
+            <FormItem className="w-full">
+              <FormLabel className="font-bold">Students Tutored</FormLabel>
+              <FormControl>
+                <Input
+                  type="number"
+                  placeholder="How many students did you tutor?"
+                  {...field}
+                />
+              </FormControl>
+              {/* <FormDescription className="font-bold">
+                  Enter the amount of students you tutored
+                </FormDescription> */}
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <div>
+          <h2 className="text-xl font-bold text-blue-400">
+            Topics and Concerns:
+          </h2>
           <div>
-            <h2 className="text-xl font-bold text-blue-400">
-              Topics and Concerns:
-            </h2>
-            <div>
-              <p className="text-sm font-semibold">
-                Fill in a topic and the relevant concerns to that topic. You can
-                add a new topic by clicking the "Add Topic" button. You can also
-                delete a topic by clicking the "Delete" button.
-              </p>
-            </div>
+            <p className="text-sm font-semibold">
+              Fill in a topic and the relevant concerns to that topic. You can
+              add a new topic by clicking the "Add Topic" button. You can also
+              delete a topic by clicking the "Delete" button.
+            </p>
           </div>
+        </div>
 
-          <div className="space-y-4">
-            {form.watch("topics").map((_, index) => (
-              <div
-                className={`${
-                  index >= 0 ? "pl-8 border-slate-200 border-l-[2px]" : "pl-0"
-                }`}
-                key={index}
-              >
-                <FormField
-                  control={form.control}
-                  name={`topics.${index}.topic`}
-                  render={({ field }) => (
-                    <FormItem className="w-full">
-                      <FormLabel className="font-bold">Topic</FormLabel>
-                      <FormControl>
-                        <Select
-                          onValueChange={field.onChange}
-                          defaultValue={""}
-                        >
-                          <SelectTrigger>
-                            <SelectValue
-                              placeholder="What topic was covered?"
-                              {...field}
-                            />
-                          </SelectTrigger>
-                          <SelectContent>
-                            <SelectItem value="Heaps">Heaps</SelectItem>
-                            <SelectItem value="Hashing">Hashing</SelectItem>
-                            <SelectItem value="Binary Search Trees">
-                              Binary Search Trees
-                            </SelectItem>
-                          </SelectContent>
-                        </Select>
-                      </FormControl>
-                      <FormDescription className="font-bold">
-                        Choose the topic you covered
-                      </FormDescription>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-
-                <FormField
-                  control={form.control}
-                  name={`topics.${index}.observations`}
-                  render={({ field }) => (
-                    <FormItem className="w-full">
-                      <FormLabel className="font-bold">Observations</FormLabel>
-                      <FormControl>
-                        <Textarea
-                          className="h-24"
-                          placeholder="One student had trouble understanding the concept of..."
-                          {...field}
-                        />
-                      </FormControl>
-                      <FormDescription className="font-bold">
-                        Write any observations, concerns, student struggles,
-                        etc.
-                      </FormDescription>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-
-                {index > 0 && (
-                  <Button
-                    type="button"
-                    onClick={() => deleteInput(index)}
-                    className="bg-red-400 hover:bg-red-500 rounded-md text-white font-semibold mt-2"
-                  >
-                    Delete
-                  </Button>
+        <div className="space-y-4">
+          {form.watch("topics").map((_, index) => (
+            <div
+              className={`${
+                index >= 0 ? "pl-8 border-slate-200 border-l-[2px]" : "pl-0"
+              }`}
+              key={index}
+            >
+              <FormField
+                control={form.control}
+                name={`topics.${index}.topic`}
+                render={({ field }) => (
+                  <FormItem className="w-full">
+                    <FormLabel className="font-bold">Topic</FormLabel>
+                    <FormControl>
+                      <Select onValueChange={field.onChange} defaultValue={""}>
+                        <SelectTrigger>
+                          <SelectValue
+                            placeholder="What topic was covered?"
+                            {...field}
+                          />
+                        </SelectTrigger>
+                        <SelectContent>
+                          <SelectItem value="Heaps">Heaps</SelectItem>
+                          <SelectItem value="Hashing">Hashing</SelectItem>
+                          <SelectItem value="Binary Search Trees">
+                            Binary Search Trees
+                          </SelectItem>
+                        </SelectContent>
+                      </Select>
+                    </FormControl>
+                    <FormDescription className="font-bold">
+                      Choose the topic you covered
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
                 )}
-              </div>
-            ))}
+              />
 
-            <Button
-              type="button"
-              onClick={addNewInputs}
-              className="bg-blue-400 hover:bg-blue-500 text-sm rounded-md text-white font-semibold"
-            >
-              Add Topic
-            </Button>
-          </div>
+              <FormField
+                control={form.control}
+                name={`topics.${index}.observations`}
+                render={({ field }) => (
+                  <FormItem className="w-full">
+                    <FormLabel className="font-bold">Observations</FormLabel>
+                    <FormControl>
+                      <Textarea
+                        className="h-24"
+                        placeholder="One student had trouble understanding the concept of..."
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormDescription className="font-bold">
+                      Write any observations, concerns, student struggles, etc.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
-          <div className="flex justify-end">
-            <Button
-              type="submit"
-              className="bg-custom-yellow hover:bg-custom-orange rounded-lg text-white font-semibold drop-shadow-xl"
-            >
-              Submit
-            </Button>
-          </div>
-        </form>
-      </div>
+              {index > 0 && (
+                <Button
+                  type="button"
+                  onClick={() => deleteInput(index)}
+                  className="bg-red-400 hover:bg-red-500 rounded-md text-white font-semibold mt-2"
+                >
+                  Delete
+                </Button>
+              )}
+            </div>
+          ))}
+
+          <Button
+            type="button"
+            onClick={addNewInputs}
+            className="bg-blue-400 hover:bg-blue-500 text-sm rounded-md text-white font-semibold"
+          >
+            Add Topic
+          </Button>
+        </div>
+
+        <div className="flex justify-end">
+          <Button
+            type="submit"
+            className="bg-custom-yellow hover:bg-custom-orange rounded-lg text-white font-semibold drop-shadow-xl"
+          >
+            Submit
+          </Button>
+        </div>
+      </form>
     </Form>
   );
 }
