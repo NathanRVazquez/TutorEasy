907852319262f890b03cb98616de19b547dac607
Shohruz Ernazarov <shohruzernazarov04@gmail.com>
2024-12-07 20:57:10 -0500

    Rewrite raw SQL queries to prisma syntax

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,23 +1,7 @@
 import React from "react";
 import AnalyticsCard from "@/components/AnalyticsCard";
 import prisma from "@/lib/db";
-import { Users } from "lucide-react";
-import { getDefaultAutoSelectFamilyAttemptTimeout } from "net";
-
-import { useSession } from "next-auth/react";
-
-// export function getUser(){
-//   const { data: session } = useSession();
-//   if(!session || !session.user){
-//     throw new Error();
-//   }
-//   return session.user.email;
-//   }
-
-//   if(!getUser()){
-//     throw new Error();
-//   }
-//   let users_email: string = getUser() as string;
+import { UserType } from "@prisma/client";
 
 export default async function DashboardHomePage() {
   // retrieve the count of all users with the role - TA
@@ -38,109 +22,223 @@ export default async function DashboardHomePage() {
   const quarter_prior_milliseconds = curr_time - 7884000000;
   const quarter_prior = new Date(quarter_prior_milliseconds);
 
-  // const Professor_ID = await prisma.users.findFirst({
-  //   where: {
-  //     Email: "nathanrvazquez@gmail.com",
-  //   },
-  // });
+  // const professors_tutors: { ta_count: bigint }[] = await prisma.$queryRaw`
+  // SELECT COUNT(*) as ta_count
+  // FROM "TutorSchedule" as ts
+  // JOIN "User" as u_ta ON u_ta."userId" = ts."tutorId"
+  // JOIN "Class" c ON ts."Class_Assigned" = c."Class_ID"
+  // JOIN "professor_classes" pc ON c."Class_ID" = pc."Class_ID"
+  // JOIN "users" u_prof ON pc."Professor_ID" = u_prof."User_ID"
+  // WHERE u_ta."UserType" = 'Tutor'
+  // AND u_prof."userId" = 'cm4eoknda0001usw0cb9ymo3k'
+  // AND u_prof."UserType" = 'Professor';
+  // `;
 
-  // if (!Professor_ID) {
-  //   throw new Error();
-  // }
+  const professorId = "cm4evzfxc0000us5kn8xyw628"; // Test professor ID from seeded data
 
-  const professors_tutors: { ta_count: bigint }[] = await prisma.$queryRaw`
-  SELECT COUNT(*) as ta_count
-  FROM "ta_schedules" as ts
-  JOIN "users" as u_ta ON u_ta."User_ID" = ts."TA_ID"
-  JOIN "classes" c ON ts."Class_Assigned" = c."Class_ID"
-  JOIN "professor_classes" pc ON c."Class_ID" = pc."Class_ID"
-  JOIN "users" u_prof ON pc."Professor_ID" = u_prof."User_ID"
-  WHERE u_ta."User_Type" = 'TA'
-  AND u_prof."User_ID" = 'cm48pblb00000buw0i0udjhzl' 
-  AND u_prof."User_Type" = 'Professor';
-  `;
+  const tutorCount = await prisma.tutorSchedule.count({
+    where: {
+      user: {
+        userType: "Tutor",
+      },
+      class: {
+        professorTeaching: {
+          some: {
+            professorId: professorId,
+            user: {
+              userType: "Professor",
+            },
+          },
+        },
+      },
+    },
+  });
 
-  const professors_tutoring_sessions: { ts_count: bigint }[] =
-    await prisma.$queryRaw`
-  SELECT COUNT(*) as ts_count
-  FROM "tutoring_sessions" as ts
-  JOIN "classes" c ON ts."Class_Tutored" = c."Class_ID"
-  JOIN "professor_classes" pc ON c."Class_ID" = pc."Class_ID"
-  JOIN "users" u_prof ON pc."Professor_ID" = u_prof."User_ID"
-  WHERE u_prof."User_ID" = 'cm48pblb00000buw0i0udjhzl' ;
-  `;
+  console.log(tutorCount);
 
-  const professors_tutoring_session_engagement: {
-    ts_attendee_count: number;
-  }[] = await prisma.$queryRaw`
-  SELECT SUM("Students_Tutored") as ts_attendee_count
-  FROM "tutoring_sessions" as ts
-  JOIN "classes" c ON ts."Class_Tutored" = c."Class_ID"
-  JOIN "professor_classes" pc ON c."Class_ID" = pc."Class_ID"
-  JOIN "users" u_prof ON pc."Professor_ID" = u_prof."User_ID"
-  WHERE u_prof."User_ID" = 'cm48pblb00000buw0i0udjhzl';
-  `;
+  // const professors_tutoring_sessions: { ts_count: bigint }[] =
+  //   await prisma.$queryRaw`
+  // SELECT COUNT(*) as ts_count
+  // FROM "tutoring_sessions" as ts
+  // JOIN "classes" c ON ts."Class_Tutored" = c."Class_ID"
+  // JOIN "professor_classes" pc ON c."Class_ID" = pc."Class_ID"
+  // JOIN "users" u_prof ON pc."Professor_ID" = u_prof."User_ID"
+  // WHERE u_prof."User_ID" = 'cm48pblb00000buw0i0udjhzl' ;
+  // `;
 
-  const num_professors_tutors: number = Number(professors_tutors[0].ta_count);
-  const num_professors_tutoring_sessions: number = Number(
-    professors_tutoring_sessions[0].ts_count
-  );
-  const num_professors_tutoring_engagement: number = Number(
-    professors_tutoring_session_engagement[0].ts_attendee_count
-  );
+  const tutoringSessionCount = await prisma.tutoringSession.count({
+    where: {
+      class: {
+        professorTeaching: {
+          some: {
+            professorId: professorId,
+          },
+        },
+      },
+    },
+  });
 
-  const num_all_tutoring_sessions = await prisma.tutoring_sessions.count();
+  console.log(tutoringSessionCount);
+
+  // const professors_tutoring_session_engagement: {
+  //   ts_attendee_count: number;
+  // }[] = await prisma.$queryRaw`
+  // SELECT SUM("Students_Tutored") as ts_attendee_count
+  // FROM "tutoring_sessions" as ts
+  // JOIN "classes" c ON ts."Class_Tutored" = c."Class_ID"
+  // JOIN "professor_classes" pc ON c."Class_ID" = pc."Class_ID"
+  // JOIN "users" u_prof ON pc."Professor_ID" = u_prof."User_ID"
+  // WHERE u_prof."User_ID" = 'cm48pblb00000buw0i0udjhzl';
+  // `;
+
+  const tutoringSessionEngagement = await prisma.tutoringSession.aggregate({
+    _sum: {
+      numStudentsTutored: true,
+    },
+    where: {
+      class: {
+        professorTeaching: {
+          some: {
+            professorId: professorId,
+          },
+        },
+      },
+    },
+  });
+  const tsAttendeeCount =
+    tutoringSessionEngagement._sum.numStudentsTutored ?? 0;
+  console.log(tutoringSessionEngagement);
+  console.log(tsAttendeeCount);
+
+  // const num_professors_tutors: number = Number(professors_tutors[0].ta_count);
+  // const num_professors_tutoring_sessions: number = Number(
+  //   professors_tutoring_sessions[0].ts_count
+  // );
+  // const num_professors_tutoring_engagement: number = Number(
+  //   professors_tutoring_session_engagement[0].ts_attendee_count
+  // );
+
+  // const num_all_tutoring_sessions = await prisma.tutoringSession.count();
 
   // all attendees for all sessions. need to filter based on the classes the professors
   // const num_all_attendees = await prisma.attendee_list.count();
 
-  const professors_tutors_month_prior: { ta_count: bigint }[] =
-    await prisma.$queryRaw`
-  SELECT COUNT(*) as TA_Count
-  FROM "ta_schedules" as ts
-  JOIN "users" as u_ta ON u_ta."User_ID" = ts."TA_ID"
-  JOIN "classes" c ON ts."Class_Assigned" = c."Class_ID"
-  JOIN "professor_classes" pc ON c."Class_ID" = pc."Class_ID"
-  JOIN "users" u_prof ON pc."Professor_ID" = u_prof."User_ID"
-  WHERE u_ta."User_Type" = 'TA'
-  AND u_prof."User_ID" = 'cm48pblb00000buw0i0udjhzl' 
-  AND u_prof."User_Type" = 'Professor'
-  AND u_ta."Join_Date" < ${month_prior};
-  `;
+  // const professors_tutors_month_prior: { ta_count: bigint }[] =
+  //   await prisma.$queryRaw`
+  // SELECT COUNT(*) as TA_Count
+  // FROM "ta_schedules" as ts
+  // JOIN "users" as u_ta ON u_ta."User_ID" = ts."TA_ID"
+  // JOIN "classes" c ON ts."Class_Assigned" = c."Class_ID"
+  // JOIN "professor_classes" pc ON c."Class_ID" = pc."Class_ID"
+  // JOIN "users" u_prof ON pc."Professor_ID" = u_prof."User_ID"
+  // WHERE u_ta."User_Type" = 'TA'
+  // AND u_prof."User_ID" = 'cm48pblb00000buw0i0udjhzl'
+  // AND u_prof."User_Type" = 'Professor'
+  // AND u_ta."Join_Date" < ${month_prior};
+  // `;
 
-  const tutoring_sessions_week_prior: { ts_count: bigint }[] =
-    await prisma.$queryRaw`
-  SELECT COUNT(*) as ts_count
-  FROM "tutoring_sessions" as ts
-  JOIN "classes" c ON ts."Class_Tutored" = c."Class_ID"
-  JOIN "professor_classes" pc ON c."Class_ID" = pc."Class_ID"
-  JOIN "users" u_prof ON pc."Professor_ID" = u_prof."User_ID"
-  WHERE u_prof."User_ID" = 'cm48pblb00000buw0i0udjhzl'
-  AND ts."Session_Start_Time" < ${week_prior} ;
-  `;
+  const tutorCountLastMonth = await prisma.tutorSchedule.count({
+    where: {
+      user: {
+        userType: UserType.Tutor, // Assuming 'TA' corresponds to 'Tutor'
+        createdAt: {
+          lt: month_prior, // 'Join_Date' corresponds to 'createdAt'
+        },
+      },
+      class: {
+        professorTeaching: {
+          some: {
+            professorId: professorId,
+            user: {
+              userType: UserType.Professor,
+            },
+          },
+        },
+      },
+    },
+  });
 
-  const session_engagement_semester_prior: { ts_attendee_count: number }[] =
-    await prisma.$queryRaw`
-  SELECT SUM("Students_Tutored") as ts_attendee_count
-  FROM "tutoring_sessions" as ts
-  JOIN "classes" c ON ts."Class_Tutored" = c."Class_ID"
-  JOIN "professor_classes" pc ON c."Class_ID" = pc."Class_ID"
-  JOIN "users" u_prof ON pc."Professor_ID" = u_prof."User_ID"
-  WHERE u_prof."User_ID" = 'cm48pblb00000buw0i0udjhzl'
-  AND ts."Session_Start_Time" < ${quarter_prior};
-  `;
+  console.log(tutorCountLastMonth);
+
+  // const tutoring_sessions_week_prior: { ts_count: bigint }[] =
+  //   await prisma.$queryRaw`
+  // SELECT COUNT(*) as ts_count
+  // FROM "tutoring_sessions" as ts
+  // JOIN "classes" c ON ts."Class_Tutored" = c."Class_ID"
+  // JOIN "professor_classes" pc ON c."Class_ID" = pc."Class_ID"
+  // JOIN "users" u_prof ON pc."Professor_ID" = u_prof."User_ID"
+  // WHERE u_prof."User_ID" = 'cm48pblb00000buw0i0udjhzl'
+  // AND ts."Session_Start_Time" < ${week_prior} ;
+  // `;
+
+  const tutoringSessionsCountLastWeek = await prisma.tutoringSession.count({
+    where: {
+      sessionStartTime: {
+        lt: week_prior,
+      },
+      class: {
+        professorTeaching: {
+          some: {
+            professorId: "cm48pblb00000buw0i0udjhzl",
+            user: {
+              userType: UserType.Professor,
+            },
+          },
+        },
+      },
+    },
+  });
+
+  console.log(tutoringSessionsCountLastWeek);
+
+  // const session_engagement_semester_prior: { ts_attendee_count: number }[] =
+  //   await prisma.$queryRaw`
+  // SELECT SUM("Students_Tutored") as ts_attendee_count
+  // FROM "tutoring_sessions" as ts
+  // JOIN "classes" c ON ts."Class_Tutored" = c."Class_ID"
+  // JOIN "professor_classes" pc ON c."Class_ID" = pc."Class_ID"
+  // JOIN "users" u_prof ON pc."Professor_ID" = u_prof."User_ID"
+  // WHERE u_prof."User_ID" = 'cm48pblb00000buw0i0udjhzl'
+  // AND ts."Session_Start_Time" < ${quarter_prior};
+  // `;
+
+  const sessionEngagementLastSemester = await prisma.tutoringSession.aggregate({
+    _sum: {
+      numStudentsTutored: true,
+    },
+    where: {
+      sessionStartTime: {
+        lt: quarter_prior,
+      },
+      class: {
+        professorTeaching: {
+          some: {
+            professorId: "cm48pblb00000buw0i0udjhzl",
+            user: {
+              userType: UserType.Professor,
+            },
+          },
+        },
+      },
+    },
+  });
+
+  console.log(sessionEngagementLastSemester);
+  const tsAttendeeCountLastSemester =
+    sessionEngagementLastSemester._sum.numStudentsTutored;
 
   // console.log(typeof(professors_tutoring_session_engagement[0].ts_attendee_count));
-  console.log(professors_tutoring_session_engagement);
-  const num_tutors_month_prior: number = Number(
-    professors_tutors_month_prior[0].ta_count
-  );
-  const num_sessions_week_prior: number = Number(
-    tutoring_sessions_week_prior[0].ts_count
-  );
-  const num_engagement_semester: number = Number(
-    session_engagement_semester_prior[0].ts_attendee_count
-  );
+  // console.log(professors_tutoring_session_engagement);
+
+  // const num_tutors_month_prior: number = Number(
+  //   professors_tutors_month_prior[0].ta_count
+  // );
+  // const num_sessions_week_prior: number = Number(
+  //   tutoring_sessions_week_prior[0].ts_count
+  // );
+  // const num_engagement_semester: number = Number(
+  //   session_engagement_semester_prior[0].ts_attendee_count
+  // );
 
   return (
     <div className="w-full">
@@ -148,24 +246,20 @@ export default async function DashboardHomePage() {
         <div className="flex flex-wrap md:flex-nowrap gap-2 md:gap-4">
           <AnalyticsCard
             title="Engagement"
-            data={num_professors_tutoring_engagement}
-            filterData={
-              num_professors_tutoring_engagement - num_engagement_semester
-            }
+            data={tsAttendeeCount}
+            filterData={tutoringSessionCount - tsAttendeeCount}
             range={"semester"}
           />
           <AnalyticsCard
             title="Active TAs"
-            data={num_professors_tutors}
-            filterData={num_professors_tutors - num_tutors_month_prior}
+            data={tutorCount}
+            filterData={tutorCount - tutorCountLastMonth}
             range={"month"}
           />
           <AnalyticsCard
             title="Tutoring Sessions"
-            data={num_professors_tutoring_sessions}
-            filterData={
-              num_professors_tutoring_sessions - num_sessions_week_prior
-            }
+            data={tutoringSessionCount}
+            filterData={tutoringSessionCount - tutoringSessionsCountLastWeek}
             range={"week"}
           />
         </div>
