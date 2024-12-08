2a7ba881d847100b624e8101d81589780b003bc1
Shohruz Ernazarov <shohruzernazarov04@gmail.com>
2024-12-08 00:38:49 -0500

    Alpha version of database

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -22,6 +22,7 @@ async function main() {
           data: {
             clerkUserId: faker.string.uuid(),
             email: faker.internet.email(),
+            emplid: faker.string.alpha({ length: 9 }),
             firstName: faker.person.firstName(),
             lastName: faker.person.lastName(),
             imageUrl: faker.image.avatar(),
@@ -38,6 +39,7 @@ async function main() {
           data: {
             clerkUserId: faker.string.uuid(),
             email: faker.internet.email(),
+            emplid: faker.string.alpha({ length: 9 }),
             firstName: faker.person.firstName(),
             lastName: faker.person.lastName(),
             imageUrl: faker.image.avatar(),
@@ -54,6 +56,7 @@ async function main() {
           data: {
             clerkUserId: faker.string.uuid(),
             email: faker.internet.email(),
+            emplid: faker.string.alpha({ length: 9 }),
             firstName: faker.person.firstName(),
             lastName: faker.person.lastName(),
             imageUrl: faker.image.avatar(),
