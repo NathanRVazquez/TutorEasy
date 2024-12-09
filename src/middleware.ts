6fa3e9cb7487b56af5431d72ba8906ec5711a58b
Shohruz Ernazarov <shohruzernazarov04@gmail.com>
2024-12-08 21:32:43 -0500

    Change middleware to add onboarding route protection after clerk sign up

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,34 @@
+import { NextRequest, NextResponse } from "next/server";
+
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
+const isOnboardingRoute = createRouteMatcher(["/onboarding"]);
 const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
 
-export default clerkMiddleware(async (auth, req) => {
-  const { userId, redirectToSignIn } = await auth();
+export default clerkMiddleware(async (auth, req: NextRequest) => {
+  const { userId, sessionClaims, redirectToSignIn } = await auth();
+
+  // For users visiting /onboarding, don't try to redirect
+  if (userId && isOnboardingRoute(req)) {
+    return NextResponse.next();
+  }
+
+  // If the user isn't signed in and the route is private, redirect to sign-in
   if (!userId && isProtectedRoute(req)) {
     // Add custom logic to run before redirecting
-    return redirectToSignIn();
+    return redirectToSignIn({ returnBackUrl: req.url });
+  }
+
+  // Catch users who do not have `onboardingComplete: true` in their publicMetadata
+  // Redirect them to the /onboading route to complete onboarding
+  if (userId && !sessionClaims?.metadata?.onboardingComplete) {
+    const onboardingUrl = new URL("/onboarding", req.url);
+    return NextResponse.redirect(onboardingUrl);
+  }
+
+  if (userId && isProtectedRoute(req)) {
+    // Add custom logic to run before redirecting
+    return NextResponse.next();
   }
 });
 
